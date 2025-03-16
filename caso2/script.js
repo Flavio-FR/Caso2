f0f0072209b9da27d0441acc5f6ec0ae50c06209
Flavio-FR <badboydevin123@hotmail.com>
2025-03-16 18:16:31 -0500

    feat: agrega la carpeta con el index

---
+++ caso2/script.js
@@ -0,0 +1,5 @@
+// Ejemplo simple: cuando se haga clic en el logo, se muestra un mensaje
+document.querySelector('.logo').addEventListener('click', function() {
+    alert('¡Has hecho clic en el logo!');
+});
+    
